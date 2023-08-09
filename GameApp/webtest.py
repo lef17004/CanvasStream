08cb37fda1084a76d2ec2cdbb07e34020f8a0ca7
michaellefevre <mbl6050@byui.edu>
2023-08-09 11:08:19 -0500

    change working directory when running

--- GameApp/webtest.py
+++ GameApp/webtest.py
@@ -1,10 +1,21 @@
 import asyncio
 import websockets
+import webbrowser
+import os
+
+current_script_directory = os.path.dirname(os.path.abspath(__file__))
+os.chdir(current_script_directory)
+path = '../CanvasApp/index.html'
+webbrowser.open('file://' + os.path.realpath(path), 2)
 
 async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send(message)
-        print(message)
+    try:
+        async for message in websocket:
+            await websocket.send(message[::-1])
+            print(message)
+    except websockets.exceptions.ConnectionClosed:
+        await websocket.wait_closed()
+        asyncio.get_event_loop().stop()
 
 start_server = websockets.serve(echo, "localhost", 8765)
 
