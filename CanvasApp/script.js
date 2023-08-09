b9ec35f7db59d396f553af5beff9160802136822
michaellefevre <mbl6050@byui.edu>
2023-08-09 12:04:58 -0500

    adds events

--- CanvasApp/script.js
+++ CanvasApp/script.js
@@ -1,4 +1,4 @@
-
+var canvas = document.getElementById("myCanvas");
 const socket = new WebSocket('ws://localhost:8765');
 
 socket.addEventListener('open', (event) => {
@@ -7,5 +7,42 @@ socket.addEventListener('open', (event) => {
 socket.addEventListener('message', (event) => {
 });
 
-sendButton.addEventListener('click', () => {
+canvas.addEventListener('click', (event) => {
+    const x = event.clientX - canvas.getBoundingClientRect().left;
+    const y = event.clientY - canvas.getBoundingClientRect().top;
+
+    const message = {
+        type: "click",
+        x: x,
+        y: y
+    };
+
+    socket.send(JSON.stringify(message));
+});
+
+document.addEventListener('keypress', (event) => {
+    const message = {
+        type: "keypress",
+        key: event.key
+    };
+
+    socket.send(JSON.stringify(message));
+});
+
+document.addEventListener('keydown', (event) => {
+    const message = {
+        type: "keydown",
+        key: event.key
+    };
+
+    socket.send(JSON.stringify(message));
+});
+
+document.addEventListener('keyup', (event) => {
+    const message = {
+        type: "keyup",
+        key: event.key
+    };
+
+    socket.send(JSON.stringify(message));
 });
