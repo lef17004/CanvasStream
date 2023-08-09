5bb17fa418af1d20c93829d909a1a924fa225e95
michaellefevre <mbl6050@byui.edu>
2023-08-09 10:22:51 -0500

    opens html on start and closes server on lost connection

--- Demo/Server/webtest.py
+++ Demo/Server/webtest.py
@@ -1,10 +1,19 @@
 import asyncio
 import websockets
+import webbrowser
+import os
+
+path = '../Client/index.html'
+webbrowser.open('file://' + os.path.realpath(path), 2)
 
 async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send(message[::-1])
-        print(message)
+    try:
+        async for message in websocket:
+            await websocket.send(message[::-1])
+            print(message)
+    except websockets.exceptions.ConnectionClosed:
+        await websocket.wait_closed()
+        asyncio.get_event_loop().stop()
 
 start_server = websockets.serve(echo, "localhost", 8765)
 
